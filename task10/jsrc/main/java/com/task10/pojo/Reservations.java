a4c233c180330f1e21c11c19d80fd0d38909cace
ShivanathDursheti <163499200+ShivanathDursheti@users.noreply.github.com>
2024-05-22 23:50:17 +0530

    Task 10 done

---
+++ task10/jsrc/main/java/com/task10/pojo/Reservations.java
@@ -0,0 +1,31 @@
+package com.task10.pojo;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import lombok.Data;
+
+@Data
+
+@DynamoDBTable(tableName = "cmtr-af3af137-Reservations-test")
+
+public class Reservations {
+
+    @DynamoDBHashKey
+
+    private String id;
+
+    private int tableNumber;
+
+    private String clientName;
+
+    private String phoneNumber;
+
+    private String date;
+
+    private String slotTimeStart;
+
+    private String slotTimeEnd;
+
+}
---
+++ task10/jsrc/main/java/com/task10/pojo/Tables.java
@@ -0,0 +1,34 @@
+package com.task10.pojo;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperFieldModel;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTyped;
+
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+
+@DynamoDBTable(tableName = "cmtr-af3af137-Tables-test")
+
+public class Tables {
+
+    @DynamoDBHashKey
+
+    private int id;
+
+    private int number;
+
+    private int places;
+
+    @DynamoDBTyped(DynamoDBMapperFieldModel.DynamoDBAttributeType.BOOL)
+
+    private boolean isVip;
+
+    private int minOrder;
+
+}
