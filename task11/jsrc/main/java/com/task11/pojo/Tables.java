38a92de159827fe0afe53a4ab311fe7f823267ff
ShivanathDursheti <163499200+ShivanathDursheti@users.noreply.github.com>
2024-05-23 13:13:16 +0530

    Task 11 done

---
+++ task11/jsrc/main/java/com/task11/pojo/Tables.java
@@ -0,0 +1,33 @@
+package com.task11.pojo;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperFieldModel;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTyped;
+
+import lombok.Data;
+
+@Data
+
+@DynamoDBTable(tableName = "cmtr-af3af137-Tables-test")
+
+public class Tables {
+
+    @DynamoDBHashKey
+
+    private int id;
+
+    private int number;
+
+    private int places;
+
+    @DynamoDBTyped(DynamoDBMapperFieldModel.DynamoDBAttributeType.BOOL)
+
+    private boolean isVip;
+
+    private int minOrder;
+
+}
